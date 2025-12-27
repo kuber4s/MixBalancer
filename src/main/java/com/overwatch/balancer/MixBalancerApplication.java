a8495204dd18a17dcd7c0c2119ac514f16519c5d
kuber4s <kuber4s@yahoo.com>
2025-12-27 06:18:29 +0300

    Initial commit

---
+++ src/main/java/com/overwatch/balancer/MixBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.overwatch.balancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixBalancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/overwatch/balancer/MixBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overwatch.balancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
